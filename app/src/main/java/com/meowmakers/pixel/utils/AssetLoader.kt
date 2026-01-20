f31be43eea609111a5e309dc34ae4cf08468fd4b
Ryan Feline <mecharyry@gmail.com>
2026-01-20 12:42:53 +0000

    Add a json asset for the top users and an asset loader to load them.

---
+++ app/src/main/java/com/meowmakers/pixel/utils/AssetLoader.kt
@@ -0,0 +1,8 @@
+import android.content.Context
+
+object AssetLoader {
+    fun loadJsonFromAssets(context: Context, fileName: String): String {
+        return context.assets.open(fileName).bufferedReader().use { it.readText() }
+    }
+}
+
