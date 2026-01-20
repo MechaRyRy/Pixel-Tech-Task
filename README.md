0f578d2b66929ee61b32ab583d21885919277802
Ryan Feline <mecharyry@gmail.com>
2026-01-20 11:55:18 +0000

    Add dependencies necessary for the task and document in the README.md.

--- README.md
+++ README.md
@@ -13,5 +13,36 @@ background threading and Flow for data streams should be allowed.
 
 **AndroidX/Jetpack Libraries:** Standard Google-maintained libraries like ViewModel,
 Navigation-Compose, and Room (for the required data persistence) as they are part of the modern
-official Android toolset.
+official Android toolset BUT no other libraries are allowed?
+
+---
+
+**Confirmed with Ben Price**
+
+> Those assumptions are correct and also Retrofit and Ktor are fine for networking should that be a
+> preference.
+
+_Ben Price_
+
+---
+
+### Specific Dependencies Added
+
+**androidx-lifecycle-viewmodel-ktx** -> Easier to launch coroutine scopes from ViewModel without
+needing to create independent scopes / manage lifecycle.
+
+**androidx.navigation.navigation-compose** -> Easier to create compose navigation, no need to create
+emit and handle state changes manually.
+
+**androidx.compose.material.material-icons-extended** -> Just for importing a few more available
+icons.
+
+**io.ktor.ktor-client-content-negotiation** -> Automatic content negotiation with ktor clients.
+
+**io.ktor.ktor-client-okhttp** -> http engine backing ktor.
+
+**io.ktor.ktor-serialization-kotlinx-json** -> JSON Parser for ktor.
+
+**org.jetbrains.kotlinx.kotlinx-serialization-json** -> Transitive dependency of
+`io.ktor.ktor-serialization-kotlinx-json` but can be imported to allow greater customization.
 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -44,17 +43,27 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.activity)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.androidx.compose.material.extended)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization)
+    implementation(libs.kotlinx.serialization.json)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
